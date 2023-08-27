bb092d124e5fda9f46d5073d94d15eba7d80eba2
Dogukan Yildirim <dyildirim76@gmail.com>
2023-08-27 05:57:27 +0300

    roll the dice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-let random_number = Math.floor(Math.random() * 6) + 1
+let img_1 = document.getElementById("img_1");
+let img_2 = document.getElementById("img_2");
+const roll_btn = document.getElementById("btn");
+
+roll_btn.addEventListener("click", function () {
+  let random_number_1 = Math.floor(Math.random() * 6) + 1;
+  let random_img_src_1 = "./assets/images/" + random_number_1 + ".png";
+  let random_number_2 = Math.floor(Math.random() * 6) + 1;
+  let random_img_src_2 = "./assets/images/" + random_number_2 + ".png";
+  img_1.setAttribute("src", random_img_src_1);
+  img_2.setAttribute("src", random_img_src_2);
+});
